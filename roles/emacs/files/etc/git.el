4f0255e90c0fc1ef3f9c5b0b3d502e01ada17862
Ben Kolera <bkolera@iseek.com.au>
2014-04-07 21:53:11 +1000

    Fixed magit and installed vmware fusion

--- roles/emacs/files/etc/git.el
+++ roles/emacs/files/etc/git.el
@@ -2,3 +2,6 @@
 
 (global-set-key (kbd "C-c g s") 'magit-status)
 (global-set-key (kbd "C-c g p") 'magit-pull)
+
+(setq magit-git-command "/usr/local/bin/git")
+(setq magit-emacsclient-executable "/opt/local/bin/emacsclient")
